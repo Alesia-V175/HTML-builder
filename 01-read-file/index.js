03fc12f1fd1deec0f633559e72abfcd08f02492c
Alesia-V175 <vaivasilisa@gmail.com>
2022-11-02 17:01:06 +0300

    refactor: implement first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 const currentFile = fs.createReadStream('text.txt', 'utf8');
 
-currentFile.on('data', (data) => console.log(data));
+currentFile.on('data', (data) =>
+    console.log(data)
+)
