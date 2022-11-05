c83c6c51cdbcf9fd2c4ed1dda3953951fcd8578d
Alesia-V175 <vaivasilisa@gmail.com>
2022-11-05 15:17:21 +0300

    fix: update url in index js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,41 +1,42 @@
 const fs = require('fs');
-const path = require("path");
-const folderName = __dirname + "/files-copy";
+const path = require('path');
+const folderName = __dirname + '/files-copy';
 
 fs.mkdir(folderName, { recursive: true }, (error) => {
     if (error) {
         console.log('Unable to create new directory: ' + error);
         throw error;
     }
-});
-//
-// fs.truncate(folderName, err => {
-//     if(err) throw err;
-//     console.log('Файл успешно очищен');
-// });
 
-fs.readdir(folderName, (error, items) => {
-    if (error) {
-        console.log('Unable to read directory: ' + error);
-        throw error;
-    }
-
-    for (let item of items) {
-        fs.unlink(path.join(folderName, item), (err) => {
-            if (err) throw err;
-        });
-    }
-
-    fs.readdir(__dirname + '/files', {withFileTypes: true}, (error, items) => {
+    fs.readdir(folderName, (error, items) => {
         if (error) {
-            console.log('Unable to get information about directory: ' + error)
+            console.log('Unable to read directory: ' + error);
             throw error;
         }
-        items.forEach(item => {
-            fs.copyFile(__dirname + "/files/"+ item.name,   folderName + "/" + item.name, (err) => {
-                if (err) throw err;
+
+        for (let item of items) {
+            fs.unlink(path.join(folderName, item), (error) => {
+                if (error) {
+                    console.log('Unable to delete file: ' + error);
+                    throw error;
+                }
             })
+        }
+
+        fs.readdir(__dirname + '/files', {withFileTypes: true}, (error, items) => {
+            if (error) {
+                console.log('Unable to get information about directory: ' + error)
+                throw error;
+            }
+            items.forEach(item => {
+                fs.copyFile(__dirname + '/files/' + item.name, folderName + '/' + item.name, (error) => {
+                    if (error) {
+                        console.log('Unable to copy file: ' + error);
+                        throw error;
+                    }
+                })
+            })
+            console.log('All files were copied');
         })
-        console.log("All files were copied");
     })
 });
